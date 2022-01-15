8411cf89620ddb6d2625962bbc39beb1b821a48e
benjaminm511 <benji.m.511@gmail.com>
2022-01-15 10:20:21 -0800

    updating create

--- main-template/netstorm-template-v1.0/register.js
+++ main-template/netstorm-template-v1.0/register.js
@@ -3,5 +3,8 @@ reg = {
         alert("Hello");
         const street = $("#street").val();
         const city = $("#city").val();
+        const state = $("#state").val();
+        const zip_code = $("#zip_code").val();
+        const uri = $("#uri").val();
     }
 }
